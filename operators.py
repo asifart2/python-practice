4f96c5afed9e5f31d040f20b3eb6ea2788c6d17b
asifart2 <mdasifakbar66@gmail.com>
2026-05-13 11:37:00 +0600

    my python codes

---
+++ operators.py
@@ -0,0 +1,40 @@
+# Arithmetic Operators
+x = 2
+y = 3
+print(x + y);
+x = x + 2
+print(x)
+x += 2
+print(x)
+x-= 2
+print(x)
+x *= 2
+print(x)
+x /=2
+print(x)
+x //= 2
+print(x)
+x **= 2
+print(x)
+# Comparison Operators
+print(x == y)
+print (x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x >= y)
+# Logical Operators
+a = True
+b = False
+print(a and b)
+print(a or b)
+print(not a)
+# Bitwise Operatiors
+x = 5 # Binary: 0101
+y = 3 #Binary: 0011
+print(x & y) #  when both bits are 1, the result is 1, otherwise it's 0 . because 0101 & 0011 = 0001 (which is 1 in decimal) 
+print(x | y) # when either bit is 1, the result is 1, otherwise it's 0. because 0101 | 0011 = 0111 (which is 7 in decimal)
+print(x ^ y) # when the bits are different, the result is 1, otherwise it's 0. because 0101 ^ 0011 = 0110 (which is 6 in decimal)
+print(~x)
+print(x >> 1)
+print(x << 1)
